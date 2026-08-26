9a47d2f548a4af479911c2e1f34fe0747adfbfc0
Quy Hoang <apple@appurunoMacBook-Pro.local>
2026-08-26 09:20:10 +0900

    feat: provision Aurora Serverless v2 cluster with Data API and isolated VPC

--- app/api/lib/api-stack.ts
+++ app/api/lib/api-stack.ts
@@ -1,14 +1,49 @@
-import { Stack, type StackProps, CfnOutput } from "aws-cdk-lib";
+import { Stack, type StackProps, CfnOutput, Duration, RemovalPolicy } from "aws-cdk-lib";
 import type { Construct } from "constructs";
 import { HttpApi, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
 import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 import * as path from "path";
 
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "DbVpc", {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: "isolated",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+    });
+
+    vpc.addInterfaceEndpoint("SecretsManagerEndpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+    });
+
+    const dbCluster = new rds.DatabaseCluster(this, "DbCluster", {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({
+        version: rds.AuroraPostgresEngineVersion.VER_16_13,
+      }),
+      writer: rds.ClusterInstance.serverlessV2("Writer"),
+      serverlessV2MinCapacity: 0,
+      serverlessV2MaxCapacity: 1,
+      serverlessV2AutoPauseDuration: Duration.minutes(5),
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+      credentials: rds.Credentials.fromGeneratedSecret("appadmin"),
+      defaultDatabaseName: "app",
+      enableDataApi: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const dbSecret = dbCluster.secret!;
 
     const httpApi = new HttpApi(this, "HttpApi", {
       apiName: "api",
--- app/api/test/api-stack.test.ts
+++ app/api/test/api-stack.test.ts
@@ -1,5 +1,5 @@
 import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 import { ApiStack } from "../lib/api-stack";
 
 describe("ApiStack", () => {
@@ -14,5 +14,33 @@ describe("ApiStack", () => {
       RouteKey: "GET /health",
     });
     template.resourceCountIs("AWS::Lambda::Function", 1);
+  });
+
+  it("provisions an Aurora Serverless v2 Postgres cluster with Data API enabled, in an isolated-subnet VPC with no NAT gateways", () => {
+    const app = new App();
+    const stack = new ApiStack(app, "TestApiStack");
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs("AWS::EC2::VPC", 1);
+    template.resourceCountIs("AWS::EC2::NatGateway", 0);
+
+    template.hasResourceProperties("AWS::RDS::DBCluster", {
+      Engine: "aurora-postgresql",
+      EnableHttpEndpoint: true,
+      ServerlessV2ScalingConfiguration: {
+        MinCapacity: 0,
+        MaxCapacity: 1,
+      },
+    });
+
+    template.hasResourceProperties("AWS::EC2::VPCEndpoint", {
+      VpcEndpointType: "Interface",
+      ServiceName: {
+        "Fn::Join": [
+          "",
+          Match.arrayWith([Match.stringLikeRegexp("secretsmanager$")]),
+        ],
+      },
+    });
   });
 });
